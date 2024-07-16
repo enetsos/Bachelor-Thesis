5bfd55046ebb528f50846083a0cd852b613cdfae
enetsos <walter.losa@supsi.ch>
2024-07-16 12:17:35 +0200

    feat: create the PWA

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { AuthProvider } from './context/LoginContext';
 import { UserProvider } from './context/UserContext';
 
@@ -20,5 +21,16 @@ root.render(
     </AuthProvider>
   </React.StrictMode>
 );
+
+// Register the service worker
+serviceWorkerRegistration.register({
+  onUpdate: (registration) => {
+    // Notify user about the update, e.g., using a custom modal or toast
+    console.log('New content is available and will be used when all tabs for this page are closed.');
+  },
+  onSuccess: (registration) => {
+    console.log('Content is cached for offline use.');
+  },
+});
 
 reportWebVitals();
